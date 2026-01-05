4d3069416a06867d75b6ffba1ab2a559d4669b5d
tabjun <jun99120@gmail.com>
2026-01-06 02:03:52 +0900

    refactor: 20260106_뉴스 기사 요약 agent_crewai 활용

---
+++ part3_2_CrewAI_News_Reader/main.py
@@ -0,0 +1,58 @@
+import dotenv
+
+dotenv.load_dotenv()
+
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
+from tools import search_tool, scrape_tool
+
+@CrewBase
+class NewsReaderAgent:
+    
+    @agent
+    def news_hunter_agent(self):
+        return Agent(
+            config = self.agents_config['news_hunter_agent'],
+            tools = [search_tool, scrape_tool],
+        )
+    
+    @agent
+    def summarizer_agent(self):
+        return Agent(
+            config = self.agents_config['summarizer_agent'],
+            tools=[search_tool],
+        )
+        
+    @agent
+    def curator_agent(self):
+        return Agent(
+            config=self.agents_config['curator_agent'],
+        )
+    
+    @task
+    def content_harvesting_task(self):
+        return Task(
+            config=self.tasks_config['content_harvesting_task'],
+        )
+    
+    @task
+    def summarization_task(self):
+        return Task(
+            config=self.tasks_config['summarization_task'],
+        )        
+        
+    @task
+    def final_report_assembly_task(self):
+        return Task(
+            config=self.tasks_config['final_report_assembly_task'],
+        )
+        
+    @crew
+    def crew(self):
+        return Crew(
+            tasks=self.tasks,
+            agents=self.agents,
+            verbose=True,
+        )
+        
+NewsReaderAgent().crew().kickoff()
