44a24e9fe8c781b496caf7e83791914189e7656c
tabjun <jun99120@gmail.com>
2026-02-01 23:35:06 +0900

    feat: Chapter 5 Content Pipeline 프로젝트 초기 설정 (CrewAI Flow)
    1. � 학습 목표: CrewAI Flow 아키텍처 이해 및 구현
    
    - Flow의 개념: 단순 Chain 방식을 넘어선 상태 관리(State Management) 및 워크플로우 제어 기능
    
    - Router 패턴: 조건에 따라 실행 경로를 동적으로 변경하는 분기(Branching) 로직 구현 (LangGraph의 대항마)
    
    - Pipeline 구조: 리서치 → 기획(라우팅) → 제작 → 검수(SEO)로 이어지는 순환형 프로세스 설계
    
    2. ⚙️ 환경 설정 (Setup)
    
    - 프로젝트 초기화: uv init을 통한 독립적인 Python 가상환경 구성
    
    - 도구 재사용: 이전 챕터에서 검증된 'Firecrawl Web Search Tool' 모듈 이식

---
+++ part3_5_content_pipeline_flow/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from part3-5-content-pipeline-flow!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ part3_5_content_pipeline_flow/tools.py
@@ -0,0 +1,59 @@
+import os, re
+
+from crewai.tools import tool
+from firecrawl import FirecrawlApp, ScrapeOptions
+
+# 웹 검색 도구 만들기
+# Firecrawl API로 search endpoint를 사용해서 일자리 검색
+# 검색 결과에서 내용 긁어와서 마크다운으로 출력하는 도구
+
+@tool
+def web_search_tool(query: str): # 쿼리 받아서 결과 생성하는 도구
+    '''
+    Web search Tool.
+    Args:
+        query (str): The query to search the web for.
+        
+    Returns
+        A list of search results with the website content in Markdown format.
+    '''
+    app = FirecrawlApp(api_key=os.getenv("FIRECRAWL_API_KEY"))
+    
+    response = app.search(
+        query=query,
+        limit=5, # 결과 받을 개수
+        scrape_options=ScrapeOptions(
+            formats=['markdown'],
+        ),
+        
+    )
+    
+    # 이 작업을 해야 LLM 결과 출력이 쓸데없는 걸 다 출력하지 않게 할 수 있음
+    # 다 출력되면 불필요한 정보 가져오는데도 토큰 사용함
+    if not response.success:
+        return 'Error using tool.'
+    
+    cleaned_chunks = []
+    
+    # 마크다운 정리
+    for result in response.data:
+        
+        title = result['title']
+        url = result['url']
+        markdown = result['markdown']
+        
+        # 슬래시와 줄바꿈, 공백 제거
+        cleaned = re.sub(r"\\+|\n+", "", markdown).strip()
+        # 결과에서 쓸모없는 링크 없애기
+        # 마크다운 링크 찾는 정규식
+        cleaned = re.sub(r"\[[^\]]+\]\([^\)]+\)|https?://[^\s]+", "", cleaned)
+        
+        cleaned_result = {
+            'title': title,
+            'url':url,
+            'markdown':cleaned,
+        }
+        
+        cleaned_chunks.append(cleaned_result)
+        
+    return cleaned_chunks
